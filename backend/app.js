74c2109ecedbcc7259c793758f43bfe76864b8c0
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2021-08-20 17:30:16 +0300

    connected to database, set .ignore and .env

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,21 @@
 const express = require("express");
-
 const app = express();
+const mongoose = require("mongoose");
 
-//TODO: port should be in .env file.
-const port = 3310;
+require("dotenv/config");
+const port = process.env.PORT;
+const connectionString = process.env.DB_STRING;
+
+//middleware
+app.use(express.json());
+
+mongoose
+  .connect(connectionString, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Connected to database"))
+  .catch((err) => console.log("Error:" + err));
 
 app.listen(port, () => {
   console.log(`Server is running on ${port} port.`);
