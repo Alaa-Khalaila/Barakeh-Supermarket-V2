5f492982cd63ce78911a320c1c7dc18c26ea8f74
Alaa-Khalaila <alkhalila.alaa@gmail.com>
2021-08-27 14:29:54 +0300

    built user api's

---
+++ backend/routes/users.js
@@ -0,0 +1,125 @@
+const { User } = require("../models/user");
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+//Get specific user.
+router.get("/:id", async (req, res) => {
+  const user = await User.find().select("-passwordHash");
+
+  if (!user) {
+    res.status(500).json({ success: false });
+  }
+
+  res.json(user);
+});
+
+//Get all users.
+router.get("/", async (req, res) => {
+  const usersList = await User.find().select("-passwordHash");
+  res.json(usersList);
+});
+
+//Register new account.
+router.post("/register", async (req, res) => {
+  let isExist = await User.findOne({ email: req.body.email });
+  if (isExist) return res.json("This email is already registered");
+
+  let user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    passwordHash: bcrypt.hashSync(req.body.password, 10),
+    phone: req.body.phone,
+    isAdmin: req.body.isAdmin,
+  });
+
+  user = await user.save();
+
+  if (!user) return res.status(400).json("The user can not be created.");
+
+  res.json(user);
+});
+
+//Login to account.
+router.post("/login", async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  const secret = process.env.secret;
+
+  if (!user) return res.status(400).send("The user not found");
+
+  if (user && bcrypt.compareSync(req.body.password, user.passwordHash)) {
+    const token = jwt.sign(
+      {
+        userId: user.id,
+        isAdmin: user.isAdmin,
+      },
+      //TODO: secret should be variable.
+      "secret",
+      { expiresIn: "1d" }
+    );
+
+    res.status(200).json({ user: user.email, token });
+  } else {
+    res.status(400).json("Password is wrong!");
+  }
+});
+
+//Get number of users.
+router.get(`/get/count`, async (req, res) => {
+  const userCount = await User.countDocuments((count) => count);
+
+  if (!userCount) {
+    res.status(500).json({ success: false });
+  }
+  res.send({
+    userCount: userCount,
+  });
+});
+
+//Delete account.
+router.delete("/:id", (req, res) => {
+  User.findByIdAndRemove(req.params.id)
+    .then((user) => {
+      if (user) {
+        return res
+          .status(200)
+          .json({ success: true, message: "the user is deleted!" });
+      } else {
+        return res
+          .status(404)
+          .json({ success: false, message: "user not found!" });
+      }
+    })
+    .catch((err) => {
+      return res.status(500).json({ success: false, error: err });
+    });
+});
+
+//Update user password.
+router.put("/:id", async (req, res) => {
+  const userExist = await User.findById(req.params.id);
+  let newPassword;
+  if (req.body.password) {
+    newPassword = bcrypt.hashSync(req.body.password, 10);
+  } else {
+    newPassword = userExist.passwordHash;
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.params.id,
+    {
+      name: req.body.name,
+      email: req.body.email,
+      passwordHash: newPassword,
+      phone: req.body.phone,
+    },
+    { new: true }
+  );
+
+  if (!user) return res.status(400).send("the user cannot be update!");
+
+  res.send(user);
+});
+
+module.exports = router;
